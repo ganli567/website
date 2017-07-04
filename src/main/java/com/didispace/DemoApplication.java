dfa3393c255d5eb99d6ae2e9f5ec65789263ddfd
xiao han <378749601@qq.com>
2017-07-04 14:06:56 +0800

    "代码改动"

--- src/main/java/com/didispace/DemoApplication.java
+++ src/main/java/com/didispace/DemoApplication.java
@@ -19,6 +19,7 @@ public class DemoApplication {
 		logger.debug("Logger Level ：DEBUG");
 		logger.info("Logger Level ：INFO");
 		logger.error("Logger Level ：ERROR");
+		logger.error("My Git ：git");
 		return "";
 	}
 
